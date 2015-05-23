28e317f4bbcbc1d00d927156e7d07707cb8ba72c
Nemesek <dnemesek@gmail.com>
2015-05-22 21:03:03 -0500

    converting to vs 2015 rc and adding nuget packages

--- OrderWorkflow/VS2015/ObserverDemo/DomainEvents/DomainEvents.cs
+++ OrderWorkflow/VS2015/ObserverDemo/DomainEvents/DomainEvents.cs
@@ -1,20 +1,21 @@
 using System;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System.Linq;
 // from http://www.udidahan.com/2009/06/14/domain-events-salvation/
 namespace DomainEvents
 {
 	public static class DomainEvents
 	{
-		[ThreadStatic] //so that each thread has its own callbacks
-		private static List<delegate> _actions = new List<delegate>();
+	    [ThreadStatic] //so that each thread has its own callbacks
+	    private static List<Delegate> _actions;// = new List<delegate>();
 		
-		public static IContainer Container {get; set;}
+		//public static IContainer Container {get; set;}
 		
 		// Registers a callback for the given domain event
 		public static void Register<T>(Action<T> callback) // where T : IDomainEvent
 		{
-			//if(_actions == null) _actions = new List<delegate>();
+			if(_actions == null) _actions = new List<Delegate>();
 			
 			_actions.Add(callback);
 		}
@@ -28,25 +29,20 @@ namespace DomainEvents
 	   //Raises the given domain event
        public static void Raise<T>(T args) //where T : IDomainEvent
        {
-//           if (Container != null)
-// 		  {
-//            	foreach(var handler in Container.ResolveAll<Handles<T>>())
-//                 {
-// 					handler.Handle(args);
-// 				}
-// 		  }
+            //           if (Container != null)
+            // 		  {
+            //            	foreach(var handler in Container.ResolveAll<Handles<T>>())
+            //                 {
+            // 					handler.Handle(args);
+            // 				}
+            // 		  }
 
-     
-          if (_actions != null)
-		  {
-              foreach (var action in _actions)
-			  {
-                	if (action is Action<T>) ((Action<T>)action)(args);	
-			  }
-  
-		  }
 
-      }
-
+            if (_actions == null) return;
+            foreach (var action1 in _actions.Select(action => action as Action<T>))
+            {
+                action1?.Invoke(args);
+            }
+        }
 	}
 }
