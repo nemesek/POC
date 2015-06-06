89989c9e40ff57a16032c454e47dd97718506b1d
nemesek <dnemesek@gmail.com>
2015-06-06 15:23:23 -0500

    added a long running task to highlight the importance of asynchrony

--- OrderWorkflow/VS2015/ObserverDemo/DomainEvents/Program.cs
+++ OrderWorkflow/VS2015/ObserverDemo/DomainEvents/Program.cs
@@ -8,8 +8,10 @@ namespace DomainEvents
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello Class");
+	    var isLongTime = false;
             DomainEvents.Subscribe<OrderCreatedEvent>(async _ => await DoSomethingAsync());
             DomainEvents.Subscribe<OrderCreatedEvent>(async _ => await DoSomethingElseAsync());
+	    DomainEvents.Subscribe<OrderCreatedEvent>(async _  => await DoSomethingLongAsync(3));
             var order = new Order();
             order.CreateOrder();
             Console.ReadLine();
@@ -38,5 +40,11 @@ namespace DomainEvents
             return true;
         }
 
+	private static async Task<bool> DoSomethingLongAsync(int second)
+	{
+		await Task.Delay(second * 1000);
+		Console.WriteLine("Okay finally done");
+		return true;
+	}
     }
 }
