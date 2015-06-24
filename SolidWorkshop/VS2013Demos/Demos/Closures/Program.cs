011ae54300754971516f43a098a2d6b2d0e8caf7
nemesek <dnemesek@gmail.com>
2015-06-24 13:56:12 -0500

    more demo prep

--- SolidWorkshop/VS2013Demos/Demos/Closures/Program.cs
+++ SolidWorkshop/VS2013Demos/Demos/Closures/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Closures
 {
@@ -21,8 +24,62 @@ namespace Closures
             // pass func (display class) to other method sig
             var otherClass = new MyOtherClass();
             otherClass.YouTellMeWhatToDo(addWithLocalVariableMyLocal);
+            // maybe better or more readable?  with this way we don't have to even have a display class because my local is passed by value
+            otherClass.YouTellMeWhatToDo(x => x + myLocal);
+
+            // now let's do it using the conventional approach with maybe uneccessary interfaces and classes
+            var getStuffDoneGuy = new GetStuffDoneGuy();
+            otherClass.YouTellMeWhatToDo(getStuffDoneGuy);
+
+            // do you even linq bro?
+            var list = new List<int> {1, 2, 4, 5, 3, 5};
+            var jackson5 = FindTheElementsWithValue5(list);
+            Debug.Assert(jackson5.Count() == 2);
+
+            var benFoldsFive = FindTheElementsWithValue5Functional(list);
+            Debug.Assert(benFoldsFive.Count() == 2);
 
 
+        }
+
+        // tell an idiot how to do it
+        static IEnumerable<int> FindTheElementsWithValue5(IEnumerable<int> numbers)
+        {
+            var elementsWith5 = new List<int>();
+
+            foreach (var x in numbers)
+            {
+                if (x == 5) elementsWith5.Add(x);
+            }
+
+            return elementsWith5;
+        }
+
+        // tell em what you want
+        static IEnumerable<int> FindTheElementsWithValue5Functional(IEnumerable<int> numbers)
+        {
+            return numbers.Where(x => x == 5).ToList();
+        }
+    }
+
+    public interface IGetStuffDone
+    {
+        int DoSomething(int x);
+        int DoSomethingElse();
+    }
+
+    public class GetStuffDoneGuy : IGetStuffDone
+    {
+        private const int MyField = 10;
+
+        public int DoSomething(int x)
+        {
+            return x + MyField;
+        }
+
+        public int DoSomethingElse()
+        {
+            return MyField;
         }
     }
 
@@ -33,9 +90,17 @@ namespace Closures
             Console.WriteLine("Doing Something from MyOtherClass");
         }
 
+        // Higher order baby
         public void YouTellMeWhatToDo(Func<int, int> doSomething)
         {
             Console.WriteLine(doSomething(30));
         }
+
+        public void YouTellMeWhatToDo(IGetStuffDone doSomething)
+        {
+            Console.WriteLine(doSomething.DoSomething(30));
+        }
     }
+
+    
 }
