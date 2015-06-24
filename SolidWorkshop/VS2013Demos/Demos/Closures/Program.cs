6927e3f14db5a3098a57578be3d2164a3f4b0268
nemesek <dnemesek@gmail.com>
2015-06-24 10:44:19 -0500

    more closures stuff

--- SolidWorkshop/VS2013Demos/Demos/Closures/Program.cs
+++ SolidWorkshop/VS2013Demos/Demos/Closures/Program.cs
@@ -6,13 +6,21 @@ namespace Closures
     {
         static void Main(string[] args)
         {
+            // static field
             Func<int, int> squareIt = x => x * x;
             Console.WriteLine(squareIt(4));
+
+            // display class due to closure
             var myLocal = 5;
+            var myOtherLocal = 10;
             Func<int, int> addWithLocalVariableMyLocal = x => x + myLocal;
             Console.WriteLine(addWithLocalVariableMyLocal(10));
-            myLocal = 10;
+            myLocal = myOtherLocal;
             Console.WriteLine(addWithLocalVariableMyLocal(10));
+
+            // pass func (display class) to other method sig
+            var otherClass = new MyOtherClass();
+            otherClass.YouTellMeWhatToDo(addWithLocalVariableMyLocal);
 
 
         }
@@ -23,6 +31,11 @@ namespace Closures
         public void DoSomething()
         {
             Console.WriteLine("Doing Something from MyOtherClass");
+        }
+
+        public void YouTellMeWhatToDo(Func<int, int> doSomething)
+        {
+            Console.WriteLine(doSomething(30));
         }
     }
 }
