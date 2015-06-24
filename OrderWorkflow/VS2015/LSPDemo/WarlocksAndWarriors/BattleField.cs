1ee1a8b17dce7f1340547fd0c4f56fb7af4e2596
nemesek <dnemesek@gmail.com>
2015-06-24 11:12:33 -0500

    prepping for demo

---
+++ OrderWorkflow/VS2015/LSPDemo/WarlocksAndWarriors/BattleField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WarlocksAndWarriors
+{
+    public class BattleField
+    {
+        // using the <fill in the blank> pattern here so I can be loosely coupled
+        public void GoIntoBattle(Player player)
+        {
+            //player.Equip(new Sword());
+            player.Equip(new Dagger());
+            //player.Equip(new Staff());
+            var damage = player.Attack();
+            Console.WriteLine("Damage is {0}", damage);
+        }
+    }
+}
--- OrderWorkflow/VS2015/LSPDemo/WarlocksAndWarriors/Program.cs
+++ OrderWorkflow/VS2015/LSPDemo/WarlocksAndWarriors/Program.cs
@@ -8,19 +8,10 @@ namespace WarlocksAndWarriors
         {
             var warrior = new Warrior();
             var warlock = new Warlock();
-            GoIntoBattle(warrior);
-            //GoIntoBattle(warlock);
+            var battleField = new BattleField();
+            battleField.GoIntoBattle(warrior);
+            //battleField.GoIntoBattle(warlock);
 
-        }
-
-        // using the strategy pattern here so I can be loosely coupled
-        static void GoIntoBattle(Player player)
-        {
-            //player.Equip(new Sword());
-            player.Equip(new Dagger());
-            //player.Equip(new Staff());
-            var damage = player.Attack();
-            Console.WriteLine("Damage is {0}", damage);
         }
     }
 }
